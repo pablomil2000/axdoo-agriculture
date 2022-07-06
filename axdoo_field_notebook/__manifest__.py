946c5f3d9a81f767071e73bd9b6aba7235a7f0c3
Manuel Calero <manuelcalerosolis@gmail.com>
2022-07-06 11:45:45 +0200

    initial commit

---
+++ axdoo_field_notebook/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Axdoo Field Notebook',
+    "version": "15.0.1.0.0",
+    'category': 'Tools',
+    'summary': 'Centralize agriculture field notebooks',
+    'description': """
+    This module offers the possibility of centralize agriculture field notebooks
+""",
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/axdoo_field_notebook_view.xml',
+    ],
+    'application': True,
+}
