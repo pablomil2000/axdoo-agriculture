8cdf5561ee5fcd352dbeed89d3ac25e930e939be
Manuel Calero <manuelcalerosolis@gmail.com>
2022-07-06 21:46:10 +0200

    year parcel

--- axdoo_field_notebook/__manifest__.py
+++ axdoo_field_notebook/__manifest__.py
@@ -11,6 +11,7 @@
     'author': 'Xtendoo',
     'depends': [
         'base',
+        'mail',
     ],
     'data': [
         'security/security.xml',
--- axdoo_field_notebook/models/field_notebook_parcel.py
+++ axdoo_field_notebook/models/field_notebook_parcel.py
@@ -1,13 +1,24 @@
 # Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
+from datetime import date
 from odoo import fields, models
 
 
 class FieldNotebookParcel(models.Model):
     _name = "field.notebook.parcel"
+    _inherit = ["mail.thread", "mail.activity.mixin"]
     _description = "Field Notebook Parcel"
 
-    name = fields.Char("Parcel Name", required=True, index=True)
-
+    name = fields.Char(
+        string="Parcel Name",
+        required=True,
+        index=True,
+        tracking=True,
+    )
+    year_harvest = fields.Integer(
+        string="Year of harvest",
+        required=True,
+        default=date.today().year
+    )
 
