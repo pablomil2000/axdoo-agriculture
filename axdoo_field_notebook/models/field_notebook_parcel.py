b1a44b2212607843197609d9ff03cab76a143480
Manuel Calero <manuelcalerosolis@gmail.com>
2022-07-28 14:31:45 +0200

    sync

--- axdoo_field_notebook/models/field_notebook_parcel.py
+++ axdoo_field_notebook/models/field_notebook_parcel.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from datetime import date
-from odoo import fields, models
+from odoo import _, fields, models
 
 
 class FieldNotebookParcel(models.Model):
@@ -21,12 +21,16 @@ class FieldNotebookParcel(models.Model):
         required=True,
         default=date.today().year
     )
-    technical_ids = fields.Many2one(
-        comodel_name='field.notebook.technical',
-        inverse_name='parcel_ids',
+    technical_ids = fields.One2many(
+        comodel_name='field.notebook.parcel.technical',
+        inverse_name='parcel_id',
         string='Technical',
         copy=True,
         auto_join=True,
+    )
+    exploitation_id = fields.Many2one(
+        comodel_name='field.notebook.exploitation',
+        string='Exploitation',
     )
 
 class FieldNotebookParcelTechnical(models.Model):
@@ -37,20 +41,22 @@ class FieldNotebookParcelTechnical(models.Model):
         comodel_name='field.notebook.technical',
         string='Technical',
         required=True,
+        ondelete='cascade',
         delegate=True,
     )
     parcel_id = fields.Many2one(
         comodel_name='field.notebook.parcel',
         string='Parcel',
         required=True,
+        ondelete='cascade',
         index=True,
         copy=False,
     )
 
-    # _sql_constraints = [
-    #     (
-    #         "parcel_uniq",
-    #         "unique(technical_id, parcel_id)",
-    #         _("This parcel already exists in this exploitation !"),
-    #     )
-    # ]
+    _sql_constraints = [
+        (
+            "technical_uniq",
+            "unique(technical_id, parcel_id)",
+            _("This technical already exists in this parcel !"),
+        )
+    ]
