a18564339d1327e8c21c7a7c2084816ae25070ce
Manuel Calero <manuelcalerosolis@gmail.com>
2022-11-30 00:05:28 +0100

    new alfinf id in sale order

--- axdoo_agriculture/models/res_partner.py
+++ axdoo_agriculture/models/res_partner.py
@@ -1,4 +1,4 @@
-res_partner.py# Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
+# Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
