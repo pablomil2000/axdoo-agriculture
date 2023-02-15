efd227bb1549b690188c86035fda79ddb4ad3220
Manuel Calero <manuelcalerosolis@gmail.com>
2023-02-15 20:15:57 +0100

    send message

--- axdoo_agriculture/models/__init__.py
+++ axdoo_agriculture/models/__init__.py
@@ -1,3 +1,4 @@
 from . import res_partner
 from . import sale_order
 from . import agriculture_format
+from . import mail_message
---
+++ axdoo_agriculture/models/mail_message.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class Message(models.Model):
+    _inherit = 'mail.message'
+    _description = 'Message'
+
+
+    def send(self):
+        for message in self:
+            if message.is_thread_message():
+                self.env[message.model].browse(message.res_id).with_context(
+                    do_not_send_copy=True
+                )._notify_thread(message)
