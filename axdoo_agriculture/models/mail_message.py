928ea97c070080fdd63ad234168559b0c524a5d1
Manuel Calero <manuelcalerosolis@gmail.com>
2023-02-16 14:05:40 +0100

    send message

--- axdoo_agriculture/models/mail_message.py
+++ axdoo_agriculture/models/mail_message.py
@@ -51,6 +51,29 @@ class Message(models.Model):
     _inherit = 'mail.message'
     _description = 'Message'
 
+    def message_send(self):
+        message = (
+            self.env["mail.message"]
+            .sudo()
+            .create(
+                {
+                    "subject": "Message test",
+                    "author_id": self.sender.id,
+                    "email_from": self.sender.email,
+                    "message_type": "comment",
+                    "model": "res.partner",
+                    "res_id": self.recipient.id,
+                    "partner_ids": [(4, self.recipient.id)],
+                    "body": "<p>This is a test message</p>",
+                }
+            )
+        )
+        if message.is_thread_message():
+            self.env[message.model].browse(message.res_id)._notify_thread(message)
+
+    print("*" * 80)
+    print("message", message)
+    print("*" * 80)
 
     def send(self):
         for message in self:
