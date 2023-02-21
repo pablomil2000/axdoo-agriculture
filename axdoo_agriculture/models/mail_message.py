f72710addeae43024323a33d9797c3dd8918776d
Manuel Calero <manuelcalerosolis@gmail.com>
2023-02-21 20:44:45 +0100

    clean code

--- axdoo_agriculture/models/mail_message.py
+++ axdoo_agriculture/models/mail_message.py
@@ -9,29 +9,6 @@ from odoo import api, models
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
-    def message_send(self):
-        message = (
-            self.env["mail.message"]
-            .sudo()
-            .create(
-                {
-                    "subject": "Message test",
-                    "author_id": self.sender.id,
-                    "email_from": self.sender.email,
-                    "message_type": "comment",
-                    "model": "res.partner",
-                    "res_id": self.recipient.id,
-                    "partner_ids": [(4, self.recipient.id)],
-                    "body": "<p>This is a test message</p>",
-                }
-            )
-        )
-        if message.is_thread_message():
-            self.env[message.model].browse(message.res_id)._notify_thread(message)
-
-        print("*" * 80)
-        print("message", message)
-        print("*" * 80)
 
     def xmlrpc_mail_message_post(self, thread_model, thread_id, body, attachment_name, attachment_encode):
         print("*" * 80)
@@ -73,45 +50,3 @@ class MailThread(models.AbstractModel):
         return thread.message_post(**{key: value for key, value in post_data.items() if
                                       key in {'attachment_ids', 'body', 'message_type', 'partner_ids', 'subtype_xmlid',
                                               'parent_id'}}).message_format()[0]
-
-
-class Message(models.Model):
-    _inherit = 'mail.message'
-    _description = 'Message'
-
-    def message_send(self):
-        message = (
-            self.env["mail.message"]
-            .sudo()
-            .create(
-                {
-                    "subject": "Message test",
-                    "author_id": 10,
-                    "email_from": "antonio@alfinf.com",
-                    "message_type": "comment",
-                    "model": "res.partner",
-                    "res_id": 4722,
-                    "partner_ids": [(4, 4722)],
-                    "body": "<p>This is a test message</p>",
-                }
-            )
-        )
-        if message.is_thread_message():
-            self.env[message.model].browse(message.res_id)._notify_thread(message)
-
-        print("*" * 80)
-        print("message", message)
-        print("*" * 80)
-
-    def send(self):
-        for message in self:
-
-            print("*" * 80)
-            print("message", message)
-            print("message.is_thread_message", message.is_thread_message())
-            print("*" * 80)
-
-            if message.is_thread_message():
-                self.env[message.model].browse(message.res_id).with_context(
-                    do_not_send_copy=True
-                )._notify_thread(message)
