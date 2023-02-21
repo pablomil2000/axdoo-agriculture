9ec4055fbe62f9da267a1064303fb5534ab69945
Manuel Calero <manuelcalerosolis@gmail.com>
2023-02-21 20:45:16 +0100

    clean code

--- axdoo_agriculture/models/mail_message.py
+++ axdoo_agriculture/models/mail_message.py
@@ -1,8 +1,6 @@
 # Copyright 2020 Manuel Calero <manuelcalero@xtendoo.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models
-
 from odoo import api, models
 
 
