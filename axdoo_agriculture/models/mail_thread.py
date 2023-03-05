94157aa47ef16277027393b3f506fcf2fb76a2c7
Manuel Calero <manuelcalerosolis@gmail.com>
2023-03-06 00:03:50 +0100

    syncro

--- axdoo_agriculture/models/mail_thread.py
+++ axdoo_agriculture/models/mail_thread.py
@@ -7,8 +7,8 @@ from odoo import api, models, fields
 class MailThread(models.AbstractModel):
     _inherit = 'mail.thread'
 
-
-    def xmlrpc_mail_message_post(self, thread_model, thread_id, body, attachment_name, attachment_encode, agriculture_type):
+    def xmlrpc_mail_message_post(self, thread_model, thread_id, body, attachment_name, attachment_encode,
+                                 agriculture_type):
         print("*" * 80)
         print("thread_model", thread_model)
         print("thread_id", thread_id)
@@ -45,8 +45,8 @@ class MailThread(models.AbstractModel):
         print("*" * 80)
 
         new_message = thread.message_post(**{key: value for key, value in post_data.items() if
-                                      key in {'attachment_ids', 'body', 'message_type', 'partner_ids', 'subtype_xmlid',
-                                              'parent_id'}}).message_format()[0]
+                                             key in {'attachment_ids', 'body', 'message_type', 'partner_ids',
+                                                     'subtype_xmlid', 'parent_id'}}).message_format()[0]
 
         if new_message:
             new_message.write({'agriculture_type', agriculture_type})
